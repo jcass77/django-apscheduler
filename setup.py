fcc9c07b495c24e1cd0b590518aec7f681aed39f
Stanislav Kaledin <skaledin@ivi.ru>
2018-06-04 17:45:59 +0300

    Release 0.2.10

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import find_packages, setup
 
 setup(
     name='django-apscheduler',
-    version='0.2.9',
+    version='0.2.10',
     description='APScheduler for Django',
     classifiers=[
         "Development Status :: 4 - Beta",
