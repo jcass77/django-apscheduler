7e65524cb174f688a5aaa2a75bea3c17fc158b00
Eric Darchis <darchis@pobox.com>
2020-10-22 22:30:00 +0200

    Squashed commit should go straight to BigAutoField

--- django_apscheduler/migrations/0001_squashed_0008_remove_djangojobexecution_started.py
+++ django_apscheduler/migrations/0001_squashed_0008_remove_djangojobexecution_started.py
@@ -23,7 +23,6 @@ class Migration(migrations.Migration):
             name='DjangoJob',
             fields=[
                 ('id', models.CharField(help_text='Unique id for this job.', max_length=255, primary_key=True, serialize=False)),
-                ('name', models.CharField(help_text='Unique name for this job.', max_length=255, unique=True)),
                 ('next_run_time', models.DateTimeField(blank=True, db_index=True, help_text='Date and time at which this job is scheduled to be executed next.', null=True)),
                 ('job_state', models.BinaryField()),
             ],
@@ -34,11 +33,10 @@ class Migration(migrations.Migration):
         migrations.CreateModel(
             name='DjangoJobExecution',
             fields=[
-                ('id', models.AutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
+                ('id', models.BigAutoField(help_text='Unique ID for this job execution.', primary_key=True, serialize=False)),
                 ('status', models.CharField(choices=[('Started execution', 'Started execution'), ('Error!', 'Error!'), ('Executed', 'Executed')], help_text='The current status of this job execution.', max_length=50)),
                 ('run_time', models.DateTimeField(db_index=True, help_text='Date and time at which this job was executed.')),
                 ('duration', models.DecimalField(decimal_places=2, default=None, help_text='Total run time of this job (in seconds).', max_digits=15, null=True)),
-                ('started', models.DecimalField(decimal_places=2, default=None, help_text='Timestamp at which this job was started.', max_digits=15, null=True)),
                 ('finished', models.DecimalField(decimal_places=2, default=None, help_text='Timestamp at which this job was finished.', max_digits=15, null=True)),
                 ('exception', models.CharField(help_text='Details of exception that occurred during job execution (if any).', max_length=1000, null=True)),
                 ('traceback', models.TextField(help_text='Traceback of exception that occurred during job execution (if any).', null=True)),
@@ -47,18 +45,5 @@ class Migration(migrations.Migration):
             options={
                 'ordering': ('-run_time',),
             },
-        ),
-        migrations.RemoveField(
-            model_name='djangojob',
-            name='name',
-        ),
-        migrations.AlterField(
-            model_name='djangojobexecution',
-            name='id',
-            field=models.BigAutoField(help_text='Unique ID for this job execution.', primary_key=True, serialize=False),
-        ),
-        migrations.RemoveField(
-            model_name='djangojobexecution',
-            name='started',
         ),
     ]
